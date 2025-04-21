3eca023b8aafe19163cd7d77931d2ee08410e9f1
Ripple Domain <mfurkandemir554223@gmail.com>
2025-04-22 02:38:48 +0300

    Add project files.

---
+++ MooncastleEditor/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MooncastleEditor.GameProject;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MooncastleEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindowLoaded;
+        }
+        private void MainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= MainWindowLoaded;
+            openProjectBrowser();
+        }
+
+        private void openProjectBrowser()
+        {
+            var projectBrowser = new ProjectBrowser();
+
+            if (projectBrowser.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
